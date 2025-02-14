30d4be976abe9cd7937f924bfbd18a0ba3687e10
sk-04112002 <saravanakumarsangilikaalai@gmail.com>
2025-02-14 07:40:10 +0530

    second-commit

--- src/Components/JAHeader/JAHeader.jsx
+++ src/Components/JAHeader/JAHeader.jsx
@@ -8,8 +8,8 @@ function JAHeader() {
         <Container>
             <Row>
                 <Col lg={4}>
-                    <p className='display-4 fw-bold head_two my-5 head_1'><span className='display-1 fw-bold r_head'>r</span>eady <span className='display-1 fw-bold r_head'>r</span>elaxed <span className='display-1 fw-bold r_head'>r</span>esolution</p>
-                    <h4 className='head_2'>JustAct is an <span className='ccolor'>Online Dispute Resolution </span>platform offered by Lex Carta Private Limited and is set up by lawyers and professionals to disrupt the way disputes are resolved today.</h4>
+                    <p className='display-4 fw-bold head_two mb-5 mt-3 head_1'><span className='display-1 fw-bold r_head'>r</span>eady <span className='display-1 fw-bold r_head'>r</span>elaxed <span className='display-1 fw-bold r_head'>r</span>esolution</p>
+                    <h4 className='head_2 mb-5'>JustAct is an <span className='ccolor'>Online Dispute Resolution </span>platform offered by Lex Carta Private Limited and is set up by lawyers and professionals to disrupt the way disputes are resolved today.</h4>
                 </Col>
                 <Col lg={8}>
                     <Image src={Header_Image} fluid className='my-5'></Image>
